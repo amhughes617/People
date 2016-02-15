a5788e03b9e1deb575b4b7b2821e553b1566f038
Alex <amhughes@g.cofc.edu>
2016-02-15 13:31:50 -0500

    Initial import

--- src/People.java
+++ src/People.java
@@ -1,5 +1,36 @@
-public class People {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
+public class People {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        HashMap personMap = readFile();
+        for (ArrayList<Person> persons : personMap.values()) {
+            Collections.sort(persons);
+        }
+        System.out.println(personMap);
+    }
+
+    public static HashMap<String, ArrayList<Person>> readFile() {
+        HashMap<String, ArrayList<Person>> personMap = new HashMap<>();
+        File f = new File("people.csv");
+        Scanner fileScanner = new Scanner(f);
+        String line = fileScanner.nextLine();
+        while (fileScanner.hasNext()) {
+            line = fileScanner.nextLine();
+            String[] columns = line.split(",");
+            Person person = new Person(Integer.valueOf(columns[0]), columns[1], columns[2], columns[3],columns[4],columns[5]);
+            if (!personMap.containsKey(person.country)) {
+                personMap.put(person.country, new ArrayList<>());
+            }
+            personMap.get(person.country).add(person);
+        }
+        return personMap;
     }
 }
+
+
+//for (ArrayList<Person> persons : personMap.values()) {
+//        Collections.sort(persons);
+//        }
